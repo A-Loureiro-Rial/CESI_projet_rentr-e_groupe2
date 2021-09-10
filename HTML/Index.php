ccc9072ee7c425720fa359396183cb2b3e477b08
GaetanGrd <gaetangerard@hotmail.fr>
2021-09-10 09:05:09 +0200

    Add footer.php

--- HTML/Index.php
+++ HTML/Index.php
@@ -52,44 +52,6 @@
         </a>
     </div>
     <!--FOOTER-->
-    <footer id="Footer" class="footer fixed-bottom" role="contentinfo">
-        <div class="footer-links">
-            <div class="container" style="visibility: inherit; opacity: 1;">
-                <div class="row">
-                    <div class="col-md-6 col-lg-3">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-notes">
-            <div class="container" style="visibility: inherit; opacity: 1;">
-                <div class="row">
-                    <div class="col-md-8">
-                        
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <span>Tous droits réservés C_easy France © 2021&nbsp;</span>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" title="Mentions légales">Mentions légales</a>
-                            </li>
-                        </ul>  
-                    </div>
-                    <div class="col-md-4">
-                        <div class="social-links list-inline-item">
-                            <p>C_easy sur les réseaux:</p>
-                            <div class="conteneurA">
-                                <a href="#" title="Suivez C_easy sur Twitter" class="social" target="_blank" ><img src="../Images/Social/TwitterLogoNoir.png" alt="social"><div class="fond_TPZ"></div></a>
-
-                                <a href="#" title="Visitez la chaine Youtube de C_easy" class="social" target="_blank" ><img src="../Images/Social/YouTubeLogoNoir.png" alt="social"><div class="fond_TPZ"></div>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer> 
+    <?php require "../HTML/Footer.php"?> 
 </body>
 </html>
